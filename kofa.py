bc1a1358de82c02a0a3b6543c085180904cab994
Körtesi Gáspár <gazs@bergengocia.net>
2010-08-11 20:36:41 +0200

    tesseractból kiköp valamit, már csak a számok nem stimmelnek(?)

--- kofa.py
+++ kofa.py
@@ -27,37 +27,35 @@ def threshold(i):
 
 def arlista_darabol(img):
   width, height = img.size
-  for sor in xrange(2, 100*width + 2, width): # keressük meg az első sort, ahol a legszélén van fekete
+
+  sortetejek = []
+  for sor in xrange(2, height*width, width): # sorhatár: fekete csík / fekete pötty ott ahol nem szokott lenni
     if img.getdata()[sor] is 0:
-      vizsgalt_sor = sor/width + 1 # az ezutáni sor alapján fogjuk vizsgálni, hol vannak az oszlophatárok
-      #print vizsgalt_sor
-      break
-  jobbszelek = []
-  for pixel in xrange( vizsgalt_sor * width, vizsgalt_sor * width + width): # oszlophatár: az itt átmenő vonalak mentén vágjuk ketté.
-    if img.getdata()[pixel] is 0:
-      jobbszelek.append(pixel % width )
-  balszelek = [0] + jobbszelek
-  oszlopok = zip(balszelek,jobbszelek)
-  arlista_oszlopok = [img.crop((x[0],vizsgalt_sor,x[1],height)) for x in oszlopok]
-  for csik in arlista_oszlopok:
-    csik.show()
-  return arlista_oszlopok
+      sortetejek.append(sor/width)
+  soraljak = sortetejek[1:]
+  sorok = zip(sortetejek, soraljak)
+  arlista_sorok = [img.crop((0,teteje,width,alja)) for teteje, alja in sorok]
+  kockak = []
+  for sor in arlista_sorok:
+    width, height = sor.size
+    balszelek = [0]
+    for i in xrange((height-1)*width, height*width):
+      if sor.getdata()[i] is 0:
+        balszelek.append(i - (height-1)*width)
+    jobbszelek = balszelek[1:]
+    oszlopok =  zip(balszelek, jobbszelek)
+    kockak.append([sor.crop((bal,0,jobb, height)) for bal, jobb in oszlopok])
+  return kockak
 
 def process_arlista(href):
   img = Image.open(StringIO.StringIO(download(href)))
   img = img.point(threshold)
   img = img.convert("1")
   csikok = arlista_darabol(img) 
-  ize = []
   for csik in csikok:
-    csik = csik.resize((csik.size[0] *2, csik.size[1]*2), Image.NEAREST)
-    kesz_darab =  tesseract.image_to_string(csik, lang='csapi').split("\n")
-    print kesz_darab
-    #print len(kesz_darab)
-    ize.append(kesz_darab)
-  kesz_arlista = zip(*ize)
-  #for sor in kesz_arlista:
-    #print sor
+    for kocka in csik:
+      kocka = kocka.resize((kocka.size[0]*2, kocka.size[1]*2), Image.NEAREST)
+      print tesseract.image_to_string(kocka, lang='csapi').replace("@", "").replace("$","")
 
 def main():
   process_arlista(get_arlistak()[0])
