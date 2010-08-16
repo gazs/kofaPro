01e289f142cc075e4a0eaf316ecaa6df9b2b9999
Körtesi Gáspár <gazs@bergengocia.net>
2010-08-16 16:32:10 +0200

    q/dátum nézet

--- appengine/main.py
+++ appengine/main.py
@@ -1,81 +1,71 @@
 #!/usr/bin/env python
-# coding: utf-8
+# coding: utf8
+import os
+
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
-
-from models import *
-from termekek import termekek
+from google.appengine.ext.webapp import template
 import urllib
 import re
-import datetime 
+import datetime
+import models
 
-def arcsik(result):
-  arak = []
-  for property in result.properties():
-    if property.startswith("ar_"):
-      arak.append( eval("result." + property) )
-  return ",".join(arak)
+def get_helyek():
+  properties = models.Csapi._properties
+  helyek = []
+  for property in properties:
+    if property.endswith("_min"):
+      helyek.append(property.replace("_min",""))
+  return helyek
 
-
-class Api(webapp.RequestHandler):
-  def get(self):
-    path = self.request.path.split("/")[2:]
-    path = [urllib.unquote(x) for x in path]
-    termek = datum = helyszin = None
-
-    piacok = ["korhazutca", "rakocziter", "fehervariut", "vamhazkrt", "bosnyakter"]
-    for i in path:
-      if i in termekek: termek = i
-      if re.match("[0-9]{4}-[0-9]{2}-[0-9]{2}", i): datum = i
-      if i in piacok: helyszin = i
-
-    # QUERY
-    query = Ar.all()
-    if termek:
-      query.filter('aru =', termek)
-    if datum:
-      query.filter('datum =', datetime.datetime.strptime(datum, '%Y-%m-%d'))
-    results = query.fetch(50)
-    # MEGJELENÍTÉS
-    self.response.headers["Content-Type"] = "text/plain"
-   # self.response.out.write("%s %s %s\n" % (termek, datum, helyszin))
-    for result in results:
-      #refaktor!!!
-      if termek and datum and helyszin:
-        self.response.out.write( eval("result.ar_" +helyszin))
-      elif not termek and datum and helyszin:
-        self.response.out.write(result.aru+",")
-        self.response.out.write(arcsik(result))
-      elif termek and datum and not helyszin:
-        self.response.out.write(arcsik(result))
-      elif termek and not datum and helyszin:
-        self.response.out.write(str(result.datum) + ",")
-        self.response.out.write(arcsik(result))
-      elif termek and not datum and not helyszin:
-        self.response.out.write(str(result.datum) + ",")
-        self.response.out.write(arcsik(result))
-      elif not termek and datum and not helyszin:
-        self.response.out.write(result.aru+",")
-        self.response.out.write(arcsik(result))
-      elif not termek and not datum and helyszin:
-        self.response.out.write(result.aru+"," + str(result.datum) +",")
-        for helyszin in piacok:
-          self.response.out.write( eval("result.ar_" + helyszin)  )
-      elif not termek and not datum and not helyszin:
-        pass
-      self.response.out.write("\n")
 class MainHandler(webapp.RequestHandler):
   def get(self):
-    self.response.out.write('Hello world!')
+    self.response.out.write("helloworld")
 
+class QHandler(webapp.RequestHandler):
+  def get(self):
+    path = self.request.path.split("/")[2:]
+    try:
+      extension = path[-1].split(".")[1]
+    except IndexError:
+      extension = "html"
+    path = [urllib.unquote(x) for x in path]
+    datum = hely = aru = None
+    helyek = get_helyek()
+    for i in path:
+      if i in helyek: hely = i
+      if re.match("[0-9]{4}-[0-9]{2}-[0-9]{2}", i): datum = datetime.datetime.strptime(i, '%Y-%m-%d').date()
+    dbg = ""
+    if datum or aru:
+      limit = 50
+      q = models.Csapi.all()
+      if datum:
+        q.filter('datum', datum)
+      if aru:
+        q.filter('aru', aru)
+      sorok = q.fetch(limit)
+      dbg = str(sorok)
+    template_values = {
+        'extension': extension,
+        'path': path,
+        'datum': datum,
+        'hely': hely,
+        'aru': aru,
+        'sorok': sorok,
+        'dbg': dbg,
+        }
+    if extension is "html":
+      templatepath = os.path.join(os.path.dirname(__file__), 'templates/index.html')
+      self.response.out.write(template.render(templatepath, template_values))
+    else:
+      self.response.out.write("plaintext,mi?")
 
 def main():
-  application = webapp.WSGIApplication([('/', MainHandler),
-                                        ('/q/?.*', Api) 
-                                       ],
-                                       debug=True)
+  application = webapp.WSGIApplication([
+                                        ('/', MainHandler),
+                                        ('/q/?.*', QHandler),
+                                       ], debug=True)
   util.run_wsgi_app(application)
-
 
 if __name__ == '__main__':
   main()
